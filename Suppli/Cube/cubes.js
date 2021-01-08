59cef1e7c58ff7fa3fea457d1a3826fc04a1b023
Matshi Matolo <65823211+matshi-m@users.noreply.github.com>
2021-01-08 16:42:18 +0200

    Update cubes.js
    added feedback on why this would throw an error

--- Suppli/Cube/cubes.js
+++ Suppli/Cube/cubes.js
@@ -1,4 +1,12 @@
 function cubes(a){
     return a*a*a;
 }
-console.log(cubes);
+console.log(cubes); //this wont return the correct answer as this function cubes() requires a parameter. 
+//the solution should look something like the below. Also the only thing that was incorrect about the question was the spelling of return
+
+function cubes1(a){
+return a**3 ;
+}
+
+cubes1(3);
+console.log(cubes1(3));
